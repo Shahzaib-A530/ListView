ada2b3880b2c4ee8d0598f77332d787d68224c74
Shahzaib-A530 <bsef19a530@pucit.edu.pk>
2022-06-01 22:19:02 +0500

    second Commit

--- ListView/app/src/main/java/com/first/listview/MainActivity.java
+++ ListView/app/src/main/java/com/first/listview/MainActivity.java
@@ -3,12 +3,26 @@ package com.first.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> studentList = new ArrayList<String> ();
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        studentList.add("Saad");
+        studentList.add("Zahid");
+        studentList.add("Saeed");
+        studentList.add("Zainab");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, studentList);
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(arrayAdapter);
     }
 }
